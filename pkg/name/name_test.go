0feb83328a8b108c0df58f76eaf6f79bdfc126cf
Harvey Lowndes <harvey.lowndes@oracle.com>
2018-07-19 13:36:41 +0100

    Bleh

--- pkg/name/name_test.go
+++ pkg/name/name_test.go
@@ -1,6 +1,7 @@
 package name
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"strings"
@@ -9,8 +10,9 @@ import (
 
 func TestBasic(t *testing.T) {
 	test := &Test{}
+	fmt.Printf("Wercker Report Directory: %s\n", os.Getenv("WERCKER_REPORT_DIR"))
 	file, _ := os.OpenFile((os.Getenv("WERCKER_REPORT_DIR") + "/" + "test.log"), os.O_WRONLY|os.O_CREATE, 0666)
 	s := CreateName(test)
-	io.Copy(file, strings.NewReader(s+"\n"+os.Getenv("WERCKER_REPORT_DIR")))
+	io.Copy(file, strings.NewReader(s))
 	t.Fail()
 }

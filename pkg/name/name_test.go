26a8109f0a7daca33cf1ea9d8eccf42e93d34997
Harvey Lowndes <harvey.lowndes@oracle.com>
2018-07-19 13:56:35 +0100

    step

--- pkg/name/name_test.go
+++ pkg/name/name_test.go
@@ -11,7 +11,7 @@ import (
 func TestBasic(t *testing.T) {
 	test := &Test{}
 	fmt.Printf("Wercker Report Directory: %s\n", os.Getenv("WERCKER_REPORT_DIR"))
-	file, _ := os.OpenFile((os.Getenv("WERCKER_REPORT_DIR") + "/" + "test.log"), os.O_WRONLY|os.O_CREATE, 0666)
+	file, _ := os.OpenFile((os.Getenv("WERCKER_REPORT_DIR") + "/" + os.Getenv("WERCKER_STEP_ID") + "/" + "test.log"), os.O_WRONLY|os.O_CREATE, 0666)
 	s := CreateName(test)
 	io.Copy(file, strings.NewReader(s))
 }

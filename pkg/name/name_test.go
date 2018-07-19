d895e64eba04d392686d152258073f5227c7bca1
Harvey Lowndes <harvey.lowndes@oracle.com>
2018-07-19 14:00:31 +0100

    Report Artifact

--- pkg/name/name_test.go
+++ pkg/name/name_test.go
@@ -11,7 +11,9 @@ import (
 func TestBasic(t *testing.T) {
 	test := &Test{}
 	fmt.Printf("Wercker Report Directory: %s\n", os.Getenv("WERCKER_REPORT_DIR"))
-	file, _ := os.OpenFile((os.Getenv("WERCKER_REPORT_DIR") + "/" + os.Getenv("WERCKER_STEP_ID") + "/" + "test.log"), os.O_WRONLY|os.O_CREATE, 0666)
+	fmt.Printf("Wercker Report Artifacts Directory: %s\n", os.Getenv("WERCKER_REPORT_ARTIFACTS_DIR"))
+	fmt.Printf("Wercker Step ID: %s\n", os.Getenv("WERCKER_STEP_ID"))
+	file, _ := os.OpenFile((os.Getenv("WERCKER_REPORT_ARTIFACTS_DIR") + "/" + "test.log"), os.O_WRONLY|os.O_CREATE, 0666)
 	s := CreateName(test)
 	io.Copy(file, strings.NewReader(s))
 }
